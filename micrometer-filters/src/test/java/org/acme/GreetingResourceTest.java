64d30382be0c43f1e54cd6410c01b2b3e2a7b551
brunobat <brunobat@gmail.com>
2025-09-01 11:06:45 +0100

    latest experimets

--- micrometer-filters/src/test/java/org/acme/GreetingResourceTest.java
+++ micrometer-filters/src/test/java/org/acme/GreetingResourceTest.java
@@ -14,7 +14,7 @@ class GreetingResourceTest {
           .when().get("/hello")
           .then()
              .statusCode(200)
-             .body(is("Hello from Quarkus REST"));
+             .body(is("Hello from Quarkus REST with op=null"));
     }
 
 }
