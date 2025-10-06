57aa711b03b7772cc221e7aee83fbdd5089c475c
brunobat <brunobat@gmail.com>
2025-10-06 11:02:33 +0100

    Custom executor improvements

--- micrometer-otel-langchain4j/src/main/java/com/brunobat/ai/common/ExecutorProducer.java
+++ micrometer-otel-langchain4j/src/main/java/com/brunobat/ai/common/ExecutorProducer.java
@@ -1,7 +1,5 @@
 package com.brunobat.ai.common;
 
-import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.binder.jvm.ExecutorServiceMetrics;
 import io.quarkus.runtime.Shutdown;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Produces;
@@ -16,13 +14,14 @@ public class ExecutorProducer {
     private ExecutorService executor;
 
     public ExecutorProducer() {
+        executor = Executors.newFixedThreadPool(4);
     }
 
     @Produces
     @ApplicationScoped // there will be only one
     @Named("customBusinessExecutor")
     public ExecutorService executorService() {
-        return Executors.newFixedThreadPool(4);
+        return executor;
     }
 
     @Shutdown
